c9fac1932f7b30233efef63029e0d3792d5180c5
Vitolds <vitolds.skutans@gmail.com>
2016-10-31 23:51:27 +0200

    Added 'users' table to the database.

--- database/databaseCreationScript.sql
+++ database/databaseCreationScript.sql
@@ -2,20 +2,25 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `java2` DEFAULT CHARACTER SET utf8 ;
-USE `java2` ;
+create database if not exists java2 default character set utf8;
+use java2;
 
--- -----------------------------------------------------
--- Table `Java2_test`.`users`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `users` ;
+-- Users table start
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `UserID` INT(11) NOT NULL AUTO_INCREMENT,
-  `FirstName` CHAR(32) NOT NULL,
-  `LastName` CHAR(32) NOT NULL,
-  PRIMARY KEY (`UserID`)
+drop table if exists users;
+
+create table if not exists users (
+	UserID int not null auto_increment,
+  FirstName varchar(20),
+  LastName varchar(20),
+  UserName varchar(16) not null,
+  PassW varchar(255) not null, -- Password hash + salt ( ~255 )
+  Email varchar(255) not null,
+  Coins float(9, 2) default 0,
+  primary key (UserID)
 )
+
+-- Users table end
 ENGINE = InnoDB
 AUTO_INCREMENT = 1002;
 
